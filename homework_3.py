5d4731f91391f4da0c3b6b3699a8c542d29ed5f7
Christina <smirnovaua3@gmail.com>
2024-03-21 01:14:35 +0200

    create calculator

--- homework_3.py
+++ homework_3.py
@@ -1,4 +1,17 @@
-num_1 = 5
-num_2 = 6
-result = num_1 == num_2
-print(result)
+number_1 = int(input("Say whole number from 1 to 10?"))
+number_2 = 2
+multiply_action = number_1 * number_2
+divide_action = number_1 / number_2
+plus_action = number_1 + number_2
+multiply_action = number_1 - number_2
+if number_1 in (2,4,6):
+    print(multiply_action)
+elif number_1 in (8,10):
+    print(divide_action)
+elif number_1 in (1,3,5):
+    print(plus_action)
+elif number_1 in (7,9):
+    print(minus_action)
+else:
+    print("The number is not recognized.Try again!")
+
